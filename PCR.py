7777c91222d9ee3002f9f56d216f8d81c3882de1
Liliana Quintero <lilianaquintero@Air-de-Liliana.uniandes.edu.co>
2018-10-29 13:25:25 -0500

    PCR falta grafica y maligno benigno

--- PCR.py
+++ PCR.py
@@ -18,7 +18,6 @@ colum_1=file_2[:,1]
 
 txt= np.genfromtxt('datos1.dat', delimiter=',', usecols=(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31))
 
-print colum_1
 colum=[]
 
 for i in range (len(txt[0])):
@@ -28,13 +27,6 @@ for i in range (len(txt[0])):
                 sum=(lista1*lista2)/(len(txt)-1)
                 suma=np.sum(sum)
                 colum.append(suma)
-
-
-
-matriz=([[colum[0],colum[1],colum[2],colum[3],colum[4],colum[5],colum[6],colum[7],colum[8],colum[9]],[colum[10],colum[11],colum[12],colum[13],colum[14],colum[15],colum[16],colum[17],colum[18],colum[19]],[colum[20],colum[21],colum[22],colum[23],colum[24],colum[25],colum[26],colum[27],colum[28],colum[29]],[colum[30],colum[31],colum[32],colum[33],colum[34],colum[35],colum[36],colum[37],colum[38],colum[39]],[colum[40],colum[41],colum[42],colum[43],colum[44],colum[45],colum[46],colum[47],colum[48],colum[49]],[colum[50],colum[51],colum[52],colum[53],colum[54],colum[55],colum[56],colum[57],colum[58],colum[59]],[colum[60],colum[61],colum[62],colum[63],colum[64],colum[65],colum[66],colum[67],colum[68],colum[69]],[colum[70],colum[71],colum[72],colum[73],colum[74],colum[75],colum[76],colum[77],colum[78],colum[79]],[colum[80],colum[81],colum[82],colum[83],colum[84],colum[85],colum[86],colum[87],colum[88],colum[89]],[colum[90],colum[91],colum[92],colum[93],colum[94],colum[95],colum[96],colum[97],colum[98],colum[99]]])
-
-print "La matriz de covarianza es:"
-print matriz
 
 matriz2=[]
 
@@ -48,10 +40,12 @@ while j<900:
 		j=j+1
 	matriz2.append(a)
 	a=[]
+print "LA matriz de covariaza es:"
 print matriz2
 #sacar autovectores y autovalores
 
 x,y= np.linalg.eig(matriz2)
+print "MEAN"
 print "radio"
 print "Autovector",y[0]," Autovalores", x[0]
 print "Textura" 
@@ -78,9 +72,10 @@ print"Los parametros mas mportantes son el radio (",x[0], ")  y la textura (", x
 
 
 
+print txt
+eje_x=np.dot(txt,y[0])
+eje_y=np.dot(txt, y[1])
 
-eje_x=np.dot(txt)
-eje_y=np.dot(txt)
-
-plt.scatter(eje_y, eje_x)
-plt.show()
+print y[0],y[1]
+#plt.scatter(eje_x, eje_y)
+#plt.show()
