49ffbc99dc65eef5f38d4d64f55c70b2ce210ce1
Liliana Quintero <lilianaquintero@Air-de-Liliana.uniandes.edu.co>
2018-10-30 13:43:08 -0500

    Fourier 2 con graficas arregladas falta labels y prints y image final

--- Fourier2D.py
+++ Fourier2D.py
@@ -11,27 +11,36 @@ transformada= np.fft.fft2(img)
 trans_shifteada= np.fft.fftshift(img) 
 frec=np.fft.fftfreq(len(transformada[0]))
 
+plt.figure()
+plt.subplot(211)
 plt.plot(frec, transformada)
+plt.subplot(212)
+plt.imshow(abs(transformada))
+plt.colorbar()
 plt.show()
 
 def filtro(trans, frec):
     for i in range(len(frec)):
 	for j in range(len(frec)):
-        	if((abs(trans[i,j])>1000) and (abs(trans[i,j])<5000)):
+        	if((abs(trans[i,j])>3000) and (abs(trans[i,j])<5000)):
             		trans[i,j]=0
     return trans
 
 f=filtro(transformada,frec)
 
-#plt.plot(frec,f)
-plt.imshow(10*np.log10(abs(f)))
+plt.figure()
+plt.subplot(211)
+plt.plot(frec, f)
+plt.subplot(212)
+plt.imshow(abs(f))
 plt.colorbar()
 plt.show()
 
+
 filt_im= np.fft.ifft(f)
 filt= np.array(filt_im)
-
-plt.imsave("bla.pdf", filt.real)
+print np.shape(filt)
+#plt.imsave("bla_1.pdf", np.real(filt))
 
 
 
