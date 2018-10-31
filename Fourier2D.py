a3b360f660ae69660b24f153cc072175d9773290
Liliana Quintero <lilianaquintero@Air-de-Liliana.uniandes.edu.co>
2018-10-31 11:43:56 -0500

    Fourier 2 arreglado

--- Fourier2D.py
+++ Fourier2D.py
@@ -1,20 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.fftpack as fft
-
+from matplotlib.colors import LogNorm
 
 img= plt.imread("arbol.png")
 print np.shape(img)
 
 transformada= np.fft.fft2(img)
-trans_shifteada= np.fft.fftshift(img) 
+shifteada= np.fft.fftshift(transformada) 
+#trans_shift=np.fft.fft2(shifteada)
 frec=np.fft.fftfreq(len(transformada[0]))
 
 plt.figure()
 plt.subplot(211)
 plt.plot(frec, transformada)
 plt.subplot(212)
-plt.imshow(abs(transformada), )
+plt.imshow(abs(shifteada),cmap="gray")
 plt.colorbar()
 plt.show()
 
@@ -26,12 +27,12 @@ def filtro(trans, frec):
     return trans
 
 f=filtro(transformada,frec)
-
+shift_f=np.fft.fftshift(f) 
 plt.figure()
 plt.subplot(211)
 plt.plot(frec, f)
 plt.subplot(212)
-plt.imshow(abs(f))
+plt.imshow(abs(shift_f), cmap="gray")
 plt.colorbar()
 plt.show()
 
@@ -39,7 +40,7 @@ plt.show()
 filt_im= np.fft.ifft2(f)
 filt= np.array(filt_im)
 print np.shape(filt)
-plt.imsave("bla_11.pdf", np.real(filt), cmap="gray")
+plt.imsave("QuinteroLiliana_Imagen_filtrada.pdf", np.real(filt), cmap="gray")
 
 
 
