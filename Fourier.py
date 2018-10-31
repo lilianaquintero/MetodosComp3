7237f595d13aeabce700bd1bcf0e475706c2d09f
Liliana Quintero <lilianaquintero@Air-de-Liliana.uniandes.edu.co>
2018-10-31 11:25:11 -0500

    Fourier con graficas guardadas labels y prints

--- Fourier.py
+++ Fourier.py
@@ -17,7 +17,7 @@ plt.plot(t,y, c="lime")
 plt.xlabel("Tiempo")
 plt.ylabel("Amplitud")
 plt.title("Datos T vs A signal.dat")
-plt.show()
+plt.savefig("QuinteroLiliana_signal.pdf")
 
 #implementacion propia Fourier:
 G_N=[]
@@ -39,7 +39,7 @@ plt.plot(h,transformada, c="darkmagenta")
 plt.xlabel("Frecuencia")
 plt.ylabel("Amplitud")
 plt.title("Transformada de Fourier Signal.dat F vs A")
-plt.show()
+plt.savefig("QuinteroLiliana_TF.pdf")
 
 print "Las frecuencias principales de la funcion son :384.261 Hz, 362.544 Hz, 139.695 Hz y 0"
 
@@ -62,7 +62,7 @@ plt.plot(t,filtrada, c="gold")
 plt.xlabel("Tiempo")
 plt.ylabel("Amplitud Filtrada")
 plt.title("Senial filtrada")
-plt.show()
+plt.savefig("QuinteroLiliana_filtrada.pdf")
 
 #datos incompletos
 dat_in= np.genfromtxt("incompletos.dat", usecols=(0,2))
@@ -70,10 +70,7 @@ dat_in= np.genfromtxt("incompletos.dat", usecols=(0,2))
 n_2 = len(dat_in) # numbero de puntos en el intervalo                                                     
 t_2= dat_in[:,0]
 y_2= dat_in[:,1]
-dt_2=t_2[1]-t_2[0]
-plt.figure()
-plt.plot(t_2,y_2)
-plt.show()                                                                                          
+dt_2=t_2[1]-t_2[0]                                                                                         
 
 #transformada datos incompletos
 G_N2=[]
@@ -89,13 +86,6 @@ for i in range (n_2):
 h_2=fft.fftfreq(n_2,dt_2)
 transformada_2=abs(np.real(G_N2))
 
-#no plotear
-plt.figure()
-plt.plot(h_2, transformada_2, c="mediumblue")
-plt.xlabel("Frecuencia")
-plt.ylabel("Amplitud")
-plt.title("Transformada de Fourier: datos incompletos")
-plt.show()
 
 print "Los datos incompletos no se pueden usar ya que NO SEEE"
 
@@ -104,7 +94,7 @@ x_interpolacion= np.linspace(dat_in[0,0], dat_in[(n_2-1),0],512)
 h_i= (dat_in[0,0] - dat_in[(n_2-1),0])/512
 fcuadratica=[]
 fcubica=[]
-#funcion que hace la interpolacion y grafica la lineal, cuadratica y cubica
+#funcion que hace la interpolacion cuadratica y cubica
 def interpolacion (dat_in, x_interpolacion,fcuadratica,fcubica): 
     x=dat_in[:,0]
     y=dat_in[:,1]
@@ -112,7 +102,6 @@ def interpolacion (dat_in, x_interpolacion,fcuadratica,fcubica):
     cubica=interpolate.interp1d(x,y, kind="cubic")
     f_cuadratica=cuadratica(x_interpolacion)
     f_cubica=cubica(x_interpolacion)
-    #convierto las interpolaciones en listas y luego agregos los valores a las listas de afuera para usarlas en punto b
     f_cuadratica=f_cuadratica.tolist()
     f_cubica=f_cubica.tolist()
     for i in range (len(f_cuadratica)):
@@ -125,17 +114,15 @@ interp= interpolacion(dat_in, x_interpolacion,fcuadratica,fcubica)
 
 fcuadratica=np.array(fcuadratica)
 fcubica=np.array(fcubica)
-print np.size(fcuadratica)
+
+#transformadas de Fourier para las interpolaciones
 
 n_3 = 512 # numbero de puntos en el intervalo                                                              
 t = dat[:,0]
 y_3= fcuadratica
 G_N3=[]
-i_s=[]
-
 for i in range (n_3):
     gn=0.0
-    i_s.append(i)
     for j in range (n):
         h= y_3[j]*np.exp(-1j*2*np.pi*j*(float(i)/n))
         gn=gn+h
@@ -163,15 +150,29 @@ h= fft.fftfreq(n,dt)
 h_4=fft.fftfreq(n_4,h_i)
 transformada_4=abs(np.real(G_N4))
 
-plt.figure()
-plt.subplot(311)
-plt.plot(h_3,transformada_3)
-plt.subplot(312)
-plt.plot(h_4,transformada_4)
-plt.subplot(313)
-plt.plot(h,transformada)
-plt.show()
+#plotear con subplots las tres transformadas de fourier
 
+plt.figure()
+plt.subplots_adjust(hspace=0.5)
+plt.subplot(311)
+plt.plot(h_3,transformada_3, label="Interp. Cuadratica", c="crimson")
+plt.xlabel("Frecuencia")
+plt.ylabel("Amplitud")
+plt.legend(loc="best")
+plt.title("Transformadas de Fourier de Interpolaciones y transformada normal")
+plt.subplot(312)
+plt.plot(h_4,transformada_4, label="Interp. Cubica", c="aqua")
+plt.xlabel("Frecuencia")
+plt.ylabel("Amplitud")
+plt.legend(loc="best")
+plt.subplot(313)
+plt.plot(h,transformada, label= "Transformada", c="indigo")
+plt.xlabel("Frecuencia")
+plt.ylabel("Amplitud")
+plt.legend(loc="best")
+plt.savefig("QuinteroLiliana_TF_interpola.pdf")
+
+#filtro para <500
 def filtro2(trans, frec):
     for i in range(len(frec)):
         if(abs(frec[i])<500):
@@ -194,17 +195,26 @@ f1_500= fft.ifft(n_filt_2)
 f2_500= fft.ifft(cua_filt_2)
 f3_500= fft.ifft(cub_filt_2)
 
-
+#plotear para ambos filtros los tres metodos
 plt.figure()
+plt.subplots_adjust(hspace=0.5)
 plt.subplot(211)
-plt.plot(t, f1, c='blue')
-plt.plot(t, f2, c='red')
-plt.plot(t, f3)
+plt.plot(t, f1, c='dodgerblue', label="Transformada")
+plt.plot(t, f2, c='chartreuse', label="Cuadratica")
+plt.plot(t, f3, 'r--', label="Cubica")
+plt.xlabel("Tiempo")
+plt.ylabel("Amplitud")
+plt.title("Senial filtrada con filtro <500 y >1000 \n Filtro >1000")
+plt.legend(loc="best")
 plt.subplot(212)
-plt.plot(t, f1_500)
-plt.plot(t, f2_500)
-plt.plot(t, f3_500)
-plt.show()
+plt.plot(t, f1_500, c='dodgerblue', label="Transformada")
+plt.plot(t, f2_500, c='chartreuse', label="Cuadratica")
+plt.plot(t, f3_500, 'r--', label="Cubica")
+plt.xlabel("Tiempo")
+plt.ylabel("Amplitud")
+plt.title("Filtro <500")
+plt.legend(loc="best")
+plt.savefig("QuinteroLiliana_2Filtros.pdf")
 
 
 
