d4f5428175711b3880ba00d20a510e49115f132d
Liliana Quintero <lilianaquintero@Air-de-Liliana.uniandes.edu.co>
2018-10-31 15:45:28 -0500

    Fourier 2 terminado

--- Fourier2D.py
+++ Fourier2D.py
@@ -4,42 +4,50 @@ import scipy.fftpack as fft
 from matplotlib.colors import LogNorm
 
 img= plt.imread("arbol.png")
-print np.shape(img)
 
-transformada= np.fft.fft2(img)
+transformada= np.fft.fft2(img) #transformada de Fourier
 shifteada= np.fft.fftshift(transformada) 
-#trans_shift=np.fft.fft2(shifteada)
 frec=np.fft.fftfreq(len(transformada[0]))
 
+#plot transformada en escala logaritmicaya que uestra bien el centro y a escala
 plt.figure()
+plt.subplots_adjust(hspace=0.5)
 plt.subplot(211)
 plt.plot(frec, transformada)
+plt.xlabel("Frecuencia")
+plt.ylabel("Amplitud")
+plt.title("Transformada de Fourier de la imagen \n Transf. Frec vs Amplitud")
 plt.subplot(212)
-plt.imshow(abs(shifteada),cmap="gray")
+plt.imshow(10*np.log10(abs(shifteada)),cmap="gray")
 plt.colorbar()
-plt.show()
+plt.savefig("QuinteroLiliana_FT2D.pdf")
 
-def filtro(trans, frec):
-    for i in range(len(frec)):
+t_filtrar=np.copy(transformada)
+#filtro para eliminar el ruido el cual a partir del plot de Ampl vs Frec s puede ver que corresponde a valores de amplitud entre 4100 y 4120
+
+for i in range(len(frec)):
 	for j in range(len(frec)):
-        	if((abs(trans[i,j])>4000) and (abs(trans[i,j])<5000)):
-            		trans[i,j]=0
-    return trans
+        	if((abs(t_filtrar[i,j])>4100) and (abs(t_filtrar[i,j])<4120)):
+            		t_filtrar[i,j]=0
 
-f=filtro(transformada,frec)
-shift_f=np.fft.fftshift(f) 
+
+shift_f=np.fft.fftshift(t_filtrar) 
+
 plt.figure()
+plt.subplots_adjust(hspace=0.5)
 plt.subplot(211)
-plt.plot(frec, f)
+plt.plot(frec, t_filtrar)
+plt.xlabel("Frecuencia")
+plt.ylabel("Amplitud")
+plt.title("Filtro de la transformada \n Filtro. Frec vs Amplitud")
 plt.subplot(212)
-plt.imshow(abs(shift_f), cmap="gray")
+plt.imshow((abs(shift_f)), cmap="gray", norm=LogNorm())
 plt.colorbar()
-plt.show()
+plt.savefig("QuinteroLiliana_FT2D_filtrada.pdf")
 
 
-filt_im= np.fft.ifft2(f)
+filt_im= np.fft.ifft2(t_filtrar)
 filt= np.array(filt_im)
-print np.shape(filt)
 plt.imsave("QuinteroLiliana_Imagen_filtrada.pdf", np.real(filt), cmap="gray")
 
 
